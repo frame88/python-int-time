6bd209db097055e4bb19424651770530b080ca0d
Francesco Leuzzi <90636442+frame88@users.noreply.github.com>
2022-02-28 19:16:46 +0100

    creazione delle 3 variabili che descrivono un orario qualunque

---
+++ 2.1.py
@@ -0,0 +1,7 @@
+print('QUESTO SCRIPT CONVERTE IL TUO ORARIO IN SECONDI')
+
+ore = int(input('quante ore devo convertire? : '))
+minuti = int(input('quanti minuti devo convertire? : '))
+secondi = int(input('quanti secondi devo convertire? : '))
+
+
