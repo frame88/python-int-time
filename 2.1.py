675adbb45e100088bb77dee80ea87208b8f94677
frame88 <fleuzzi97@gmail.com>
2022-02-28 19:27:15 +0100

    variabile contenente l'equivalenza secondi-ore secondi-minuti

--- 2.1.py
+++ 2.1.py
@@ -4,4 +4,7 @@ ore = int(input('quante ore devo convertire? : '))
 minuti = int(input('quanti minuti devo convertire? : '))
 secondi = int(input('quanti secondi devo convertire? : '))
 
+risultato = ore*60*60 + minuti*60 + secondi
+
+
 
